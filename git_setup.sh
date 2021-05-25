4175f6507a6a89a479a0149e53dc649ee907bfae
vagrant <vagrant@localhost.localdomain>
2021-05-25 12:48:59 +0000

    fix:git_setupでwgetをyumでinstallするように修正

--- git_setup.sh
+++ git_setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # https://qiita.com/sirone/items/2e233ab9697a030f1335
-sudo yum -y install gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
+sudo yum -y install wget gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
 sudo yum -y remove git
 wget https://www.kernel.org/pub/software/scm/git/git-2.30.2.tar.gz
 tar -zxf git-2.30.2.tar.gz
